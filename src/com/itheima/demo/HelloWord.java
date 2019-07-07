237d55a8aa4bbed656a024e9ebb834a042f0df9e
liyuanxin <1079626511@qq.com>
2019-07-08 00:05:32 +0800

    提交文件到版本库

---
+++ src/com/itheima/demo/HelloWord.java
@@ -0,0 +1,10 @@
+package com.itheima.demo;
+
+import java.util.Scanner;
+
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("我草泥马");
+        Scanner scanner = new Scanner(System.in);
+    }
+}
