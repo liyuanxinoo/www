b4a719312ba75bc1126d7ed834b00b780ca87c82
liyuanxin <1079626511@qq.com>
2019-07-08 00:21:09 +0800

    提交文件到版本库001

--- src/com/itheima/demo/HelloWord.java
+++ src/com/itheima/demo/HelloWord.java
@@ -6,5 +6,11 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("我草泥马");
         Scanner scanner = new Scanner(System.in);
+
+    }
+
+    public void coobook(){
+        int i = 10;
+        System.out.println(i);
     }
 }
