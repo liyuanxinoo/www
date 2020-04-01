2a5c20785f13f4b347a4a5029f842d66a8febaf7
liyuanxin <1079626511@qq.com>
2020-04-01 14:25:12 +0800

    sss

---
+++ sss/src/main/java/com/itcast/helloword.java
@@ -0,0 +1,11 @@
+package com.itcast;
+
+/**
+ * @author Mir.Li
+ * @date 2020/4/1  -  14:17
+ */
+public class helloword {
+    public static void main(String[] args) {
+        System.out.println("hellow");
+    }
+}
